1d62b66caab6742bd7c88647895a7aaf10c07786
Matthew Palmer <matthewpalmer2209@gmail.com>
2025-08-08 22:16:15 +0100

    Add project files.

---
+++ AndroidWebViewer/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace AndroidWebViewer
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ AndroidWebViewer/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace AndroidWebViewer
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
